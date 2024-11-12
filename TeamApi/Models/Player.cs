6140204deee8041e0639931d029674026841da1a
FZoltan40 <farkasz@kkszki.hu>
2024-11-12 11:45:41 +0100

    Modell and context files are ready via EntityFramework.

---
+++ TeamApi/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeamApi.Models;
+
+public partial class Player
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Height { get; set; }
+
+    public int? Weight { get; set; }
+
+    public DateTime? CreatedTime { get; set; }
+}
---
+++ TeamApi/Models/TeamContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TeamApi.Models;
+
+public partial class TeamContext : DbContext
+{
+    public TeamContext()
+    {
+    }
+
+    public TeamContext(DbContextOptions<TeamContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Player> Players { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;database=team;user=root;password=;sslmode=none;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Player>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("player");
+
+            entity.Property(e => e.CreatedTime)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Height)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)");
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .HasDefaultValueSql("'NULL'");
+            entity.Property(e => e.Weight)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
